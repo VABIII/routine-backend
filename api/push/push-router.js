4ed64cd8ffe6b1c823ce85d753fe6b9cbaf6bf3d
VABIII <tbengtson82@gmail.com>
2022-05-11 19:01:03 -0500

    changed a lot of shit. DB redesign

--- api/push/push-router.js
+++ api/push/push-router.js
@@ -1,7 +1,22 @@
 const router = require('express').Router();
+const Push = require('./push-model');
+
+router.post('/add', (req, res, next) => {
+    const newPush = req.body;
+
+    Push.addPush(newPush)
+        .then(resp => {
+            res.status(201).json({message: "Added", resp})
+        })
+        .catch(next)
+
+})
+
+router.get('/:user_id', (req, res, next) => {
+    const { user_id } = req.params
 
 
-
+})
 
 
 
