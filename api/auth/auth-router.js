e5dc8825ccc68872e0ecedc7e1ba94ee142a27c3
VABIII <tbengtson82@gmail.com>
2022-08-13 20:49:46 -0400

    Added userRouter to project

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,57 +1,57 @@
-const router = require('express').Router();
-const bcrypt = require('bcryptjs');
-
-const jwt = require('jsonwebtoken');
-const TOKEN_SECRET = 'secret';
-const { added, checkUsername } = require('./auth-middleware');
-
-
-const buildToken = user => {
-    const payload = {
-        subject: user.id,
-        username: user.username,
-        role: user.role,
-    }
-    const options = {
-        expiresIn: '1d',
-    }
-    return jwt.sign(payload, TOKEN_SECRET, options);
-}
-
-router.post('/register', added, async (req, res, next) => {
-
-})
-
-
-router.post('/login',   checkUsername, (req, res, next) => {
-    let { username, password } = req.body;
-    let user = req.user;
-
-    if(user && bcrypt.compareSync(password, user.password)) {
-        console.log(user)
-        const token = buildToken({...user, role: "user"})
-        res.status(200).json({message: `Welcome back ${user.username}`, token, user})
-    }
-    else next({status: 401, message: 'Invalid Credentials'})
-})
-
-
-
-
-module.exports = router;
-
-
-
-
-
-
-
-
-
-
-
-
-
+// const router = require('express').Router();
+// const bcrypt = require('bcryptjs');
+//
+// const jwt = require('jsonwebtoken');
+// const TOKEN_SECRET = 'secret';
+// const { added, checkUsername } = require('./auth-middleware');
+//
+//
+// const buildToken = user => {
+//     const payload = {
+//         subject: user.id,
+//         username: user.username,
+//         role: user.role,
+//     }
+//     const options = {
+//         expiresIn: '1d',
+//     }
+//     return jwt.sign(payload, TOKEN_SECRET, options);
+// }
+//
+// router.post('/register', added, async (req, res, next) => {
+//
+// })
+//
+//
+// router.post('/login',   checkUsername, (req, res, next) => {
+//     let { username, password } = req.body;
+//     let user = req.user;
+//
+//     if(user && bcrypt.compareSync(password, user.password)) {
+//         console.log(user)
+//         const token = buildToken({...user, role: "user"})
+//         res.status(200).json({message: `Welcome back ${user.username}`, token, user})
+//     }
+//     else next({status: 401, message: 'Invalid Credentials'})
+// })
+//
+//
+//
+//
+// module.exports = router;
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
 
 
 
--- api/user/userRouter.js
+++ api/user/userRouter.js
@@ -1,9 +1,16 @@
+const router = require('express').Router();
 
+router.get('/', (req, res, next)=> {
+    res.json({message: 'HEY YOU GUYS!!!!'})
+})
 
+router.get('/:id', (req, res, next)=> {
+    const { id } = req.params;
 
+    res.json({message: `HEY ${id}!!!!`})
+})
 
-
-
+module.exports = router;
 
 
 
