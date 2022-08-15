a5a0cc9d7ee426263dcf1cf18f4614dcaac8dfd5
VABIII <tbengtson82@gmail.com>
2022-08-15 18:35:52 -0400

    Added additional seed data

--- api/weight/weight-model.js
+++ api/weight/weight-model.js
@@ -1,9 +1,28 @@
+const db = require('../../data/db-config');
+
+const getRoutineByUserId = userId => {
+// const getRoutineByUserId = (userId, type) => {
+
+    const routine = db('user AS u')
+        .innerJoin('exercise AS e', 'u.userId', 'e.userId')
+        .innerJoin('weight AS w', 'e.exerciseId', 'w.exerciseId')
+        .innerJoin('exerciseType AS et', 'e.exerciseId', 'et.exerciseId')
+        .select('u.userId', 'u.fullname', 'u.role', 'e.exerciseId', 'et.type',
+            'et.exerciseName', 'w.maxWeight')
+        .where({'u.userId': userId, 'et.type': 'Push'})
+        // .where({'u.userId': userId, 'et.type': type})
+
+    return routine;
+
+
+}
 
 
 
 
-
-
+module.exports = {
+  getRoutineByUserId,
+};
 
 
 
--- api/weight/weight-router.js
+++ api/weight/weight-router.js
@@ -1,10 +1,26 @@
 const router = require('express').Router();
+const Routines = require('./weight-model');
 
 router.get('/', (req, res, next) => {
     res.json({ message: 'HEY YOU GUYS!!!!' })
 })
 
+/** Returns a users routine by userId and exerciseType **/
+router.get('/:id', (req, res, next) => {
+    const { id } = req.params;
 
+    Routines.getRoutineByUserId(id)
+        .then(routine => res.json(routine))
+        .catch(next)
+})
+
+/** Adds a new user routine **/
+router.post('/:addRoutine', (req, res, next) => {
+
+    Routines.getRoutineByUserId(id)
+        .then(routine => res.json(routine))
+        .catch(next)
+})
 
 
 
