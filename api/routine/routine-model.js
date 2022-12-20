0eb70786415c2a4913de17571ff36b27ac178263
VABIII <plybyvern6@aol.com>
2022-12-20 11:57:47 -0500

    changes made

--- api/routine/routine-model.js
+++ api/routine/routine-model.js
@@ -6,25 +6,31 @@ const exerciseTypeDb = 'exerciseType';
 const weightDb = 'weight';
 
 
-const getRoutineByUserId = async userId => {
+const getAllRoutinesByUserId = async userId => {
 
     return db('user AS u')
         .join('weight AS w', 'u.userId', 'w.userId')
         .join('exercise AS e', 'w.exerciseId', 'e.exerciseId')
         .join('exerciseType AS et', 'e.typeId', 'et.typeId')
         .where('u.userId', userId)
-        .select('')
+        .select('e.exerciseName', 'et.type', 'w.maxWeight')
+}
+
+const getRoutineByType = async (userId, typeId) => {
+    return db('user AS u')
+        .join('weight AS w', 'u.userId', 'w.userId')
+        .join('exercise AS e', 'w.exerciseId', 'e.exerciseId')
+        .join('exerciseType AS et', 'e.typeId', 'et.typeId')
+        .where('u.userId', userId)
+        .where('et.typeId', typeId)
+        .select('e.exerciseName', 'et.type', 'w.maxWeight')
 }
 
 
 
-
-
-
-
-
 module.exports = {
-    getRoutineByUserId,
+    getAllRoutinesByUserId,
+    getRoutineByType,
 };
 
 
--- api/routine/routine-router.js
+++ api/routine/routine-router.js
@@ -10,15 +10,26 @@ router.get('/', (req, res, next) => {
 router.get('/:id', (req, res, next) => {
     const { id } = req.params;
 
-    Routines.getRoutineByUserId(id)
+    Routines.getAllRoutinesByUserId(id)
         .then(routine => res.json(routine))
-        .catch(next)
+        .catch(next);
+})
+
+/** Returns a user routine by type **/
+
+router.get('/:userId/:typeId', (req, res, next) => {
+    const {userId, typeId} = req.params;
+
+    Routines.getRoutineByType(userId, typeId)
+        .then(routine => res.json(routine))
+        .catch(next);
+
 })
 
 /** Adds a new user routine **/
 router.post('/:addRoutine', (req, res, next) => {
 
-    Routines.getRoutineByUserId(id)
+    Routines.getAllRoutinesByUserId
         .then(routine => res.json(routine))
         .catch(next)
 })
