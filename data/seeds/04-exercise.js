157c78ec01ab665067ea1472e30bcaaf2da471cc
VABIII <plybyvern6@aol.com>
2022-12-30 15:04:09 -0500

    changes made

--- data/seeds/04-exercise.js
+++ data/seeds/04-exercise.js
@@ -10,16 +10,19 @@ exports.seed = function(knex) {
           {exerciseId: 3, exerciseName: 'Arnold Shoulder Press', typeId: 1},
           {exerciseId: 4, exerciseName: 'Incline Bench Press', typeId: 1},
           {exerciseId: 5, exerciseName: 'Tricep Push Down', typeId: 1},
-          {exerciseId: 6, exerciseName: 'Tricep Extensions', typeId: 1},
+          {exerciseId: 6, exerciseName: 'Tricep Extension', typeId: 1},
           {exerciseId: 7, exerciseName: 'Lat Pull Down', typeId: 2},
           {exerciseId: 8, exerciseName: 'Bent Over Row', typeId: 2},
-          {exerciseId: 9, exerciseName: 'Reverse Flys', typeId: 2},
-          {exerciseId: 10, exerciseName: 'Tricep Extensions', typeId: 2},
-          {exerciseId: 11, exerciseName: 'Dumb Bell Curls', typeId: 2},
-          {exerciseId: 12, exerciseName: 'Z Bar Curls', typeId: 2},
-          {exerciseId: 13, exerciseName: 'Rear Squats', typeId: 3},
-          {exerciseId: 14, exerciseName: 'Deadlift', typeId: 3},
-
+          {exerciseId: 9, exerciseName: 'Reverse Fly', typeId: 2},
+          {exerciseId: 10, exerciseName: 'Upright Row', typeId: 2},
+          {exerciseId: 11, exerciseName: 'Dumb Bell Curl', typeId: 2},
+          {exerciseId: 12, exerciseName: 'Hammer Curl', typeId: 2},
+          {exerciseId: 13, exerciseName: 'Z Bar Curl', typeId: 2},
+          {exerciseId: 14, exerciseName: 'Rear Squat', typeId: 3},
+          {exerciseId: 15, exerciseName: 'Deadlift', typeId: 3},
+          {exerciseId: 16, exerciseName: 'Leg Press', typeId: 3},
+          {exerciseId: 17, exerciseName: 'Front Squat', typeId: 3},
+          {exerciseId: 18, exerciseName: 'Hack Squat', typeId: 3},
       ]);
     });
 };
--- helpers/routineHelpers.js
+++ helpers/routineHelpers.js
@@ -4,9 +4,24 @@ const exerciseTypes = {
     ArnoldPress: 3,
     InclineBenchPress: 4,
     TricepPushDown: 5,
+    TricepExtensions: 6,
+    LatPullDown: 7,
+    BentOverRow: 8,
+    ReverseFlys: 9,
+    UpRightRow: 10,
+    DumbBellCurl: 11,
+    HammerCurl: 12,
+    ZBarCurl: 13,
+    RearSquat: 14,
+    DeadLift: 15,
+    LegPress: 16,
+    FrontSquat: 17,
+    HackSquat: 18,
 };
 
+const routineBuilder = () => {
 
+};
 
 
 
