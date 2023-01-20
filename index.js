5d51676c9f7e9857f6efe5d4f072e7abd25f75b9
VABIII <plybyvern6@aol.com>
2023-01-20 15:42:20 -0500

    changes made

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const server = require('./api/server');
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 5000
 
 server.listen(port, () => {
     console.log(`running on port ${port}`)
