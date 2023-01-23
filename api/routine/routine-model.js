507c312926639fee00d828f04cc434ddc6e301e6
VABIII <plybyvern6@aol.com>
2023-01-23 11:31:48 -0500

    changes made

--- api/routine/routine-model.js
+++ api/routine/routine-model.js
@@ -1,5 +1,4 @@
 const DB = require("../../data/db-config");
-const db = require("../../data/db-config");
 const USER_DB = 'user';
 const EXERCISE_DB = 'exercise';
 const EXERCISE_TYPE_DB = 'exerciseType';
@@ -7,13 +6,12 @@ const WEIGHT_DB = 'weight';
 
 const getRoutines = async userId => {
 
-    return db(`${USER_DB} AS u`)
+    return DB(`${USER_DB} AS u`)
         .join('weight AS w', 'u.userId', 'w.userId')
         .join('exercise AS e', 'w.exerciseId', 'e.exerciseId')
         .join('exerciseType AS et', 'e.typeId', 'et.typeId')
         .where('u.userId', userId)
         .select('e.exerciseName', 'et.type', 'w.maxWeight');
-
 }
 
 const getUserRoutineByType = async (userId, typeId) => {
